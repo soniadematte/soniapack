91e0df9f0f9696185e0515935e31503ea2e371c8
soniadematte <sonia.dema27@gmail.com>
2024-12-18 14:31:13 +0100

    simplestuff.R

---
+++ R/simplestuff.R
@@ -0,0 +1,11 @@
+# The function to make a function is called 'function'
+# We have to put the argument (x,y)
+# we have to use the {} to make R know that there the function has to do those things
+# We have to assign a name to the function
+
+simplestuff <- function(x,y){
+  z=x+y
+  return(z)
+  }
+
+# We put the function in R, and assign numbers to x and y
